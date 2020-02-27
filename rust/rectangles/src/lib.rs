606922c517370596ddaf7faf35b312ab1492d409
Gökmen Görgen <gkmngrgn@gmail.com>
2020-02-27 22:03:15 +0100

    exercism-rust, rectangles: update solution.

--- rust/rectangles/src/lib.rs
+++ rust/rectangles/src/lib.rs
@@ -1,11 +1,6 @@
 #[macro_use]
 extern crate itertools;
 
-const CORNER: char = '+';
-const LINE_H: char = '-';
-const LINE_V: char = '|';
-const SPACE: char = ' ';
-
 pub fn count(lines: &[&str]) -> u32 {
     let mut counter = 0;
     let horizontal_lines = lines.into_iter().map(|l| l.chars().collect()).collect();
@@ -17,7 +12,7 @@ pub fn count(lines: &[&str]) -> u32 {
             row.chars()
                 .enumerate()
                 .filter_map(move |(x, col)| match col {
-                    CORNER => Some((x, y)),
+                    '+' => Some((x, y)),
                     _ => None,
                 })
         })
@@ -27,10 +22,10 @@ pub fn count(lines: &[&str]) -> u32 {
         let v = get_slice(&vertical_lines, left, right, top, bottom);
         if !h.is_empty()
             && !v.is_empty()
-            && !h.contains(SPACE)
-            && !v.contains(SPACE)
-            && !h.contains(LINE_V)
-            && !v.contains(LINE_H)
+            && !h.contains(' ')
+            && !v.contains(' ')
+            && !h.contains('|')
+            && !v.contains('-')
         {
             counter += 1;
         }
