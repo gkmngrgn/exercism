855d770df37d866dd8086b4442b733e1e6d840a6
Gökmen Görgen <gkmngrgn@gmail.com>
2020-03-01 01:22:04 +0100

    dart, isbn-verifier: update solution.

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,9 +1,10 @@
 bool isValid(String isbn) {
-  if (RegExp(r"^[0-9-]{9,}?[0-9X]{1}$").stringMatch(isbn) != isbn) return false;
-  List<String> digits = isbn.replaceAll("-", "").split("").reversed.toList();
-  int total = 0;
-  for (int i = 0; i < digits.length; i++) {
-    total += (int.tryParse(digits[i]) ?? 10) * (i + 1);
-  }
+  if (RegExp(r'^[0-9-]{9,}?[0-9X]{1}$').stringMatch(isbn) != isbn) return false;
+  var digits = isbn.replaceAll('-', '').split('').reversed.toList();
+  var total = digits
+      .asMap()
+      .map((i, d) => MapEntry(i, (int.tryParse(d) ?? 10) * (i + 1)))
+      .values
+      .reduce((a, b) => a + b);
   return total % 11 == 0 && digits.length == 10;
 }
