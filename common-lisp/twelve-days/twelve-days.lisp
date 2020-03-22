f189725e819869893c0995829af4d768fa5fc727
Gökmen Görgen <gkmngrgn@gmail.com>
2020-03-22 21:18:28 +0100

    common-lisp, twelve-days: new solution.

--- common-lisp/twelve-days/twelve-days.lisp
+++ common-lisp/twelve-days/twelve-days.lisp
@@ -13,8 +13,8 @@
     ("fifth"    . "five Gold Rings")
     ("sixth"    . "six Geese-a-Laying")
     ("seventh"  . "seven Swans-a-Swimming")
-    ("eigth"    . "eight Maids-a-Milking")
-    ("nineth"   . "nine Ladies Dancing")
+    ("eighth"   . "eight Maids-a-Milking")
+    ("ninth"    . "nine Ladies Dancing")
     ("tenth"    . "ten Lords-a-Leaping")
     ("eleventh" . "eleven Pipers Piping")
     ("twelfth"  . "twelve Drummers Drumming")))
@@ -22,17 +22,25 @@
 (defun recite (&optional begin end)
   (labels ((verse (index)
              (format nil
-                     "On the ~a day of Christmas my true love gave to me: ~a"
+                     "On the ~a day of Christmas my true love gave to me: ~a."
                      (get-ordinal-numeral-as-str index)
-                     (get-goods-list index)))
+                     (get-goods index)))
            (get-ordinal-numeral-as-str (index)
              (car (nth (- index 1) days-and-goods)))
            (get-goods (index)
-             (if (= index 1)
-                 (cdr (nth 1 days-and-goods))
-                 (loop for ))
-             (loop for i ...))
-           ))
-    (loop for i from begin
-       below end
-       collect (verse i))))
+             (reduce (lambda (all-goods i)
+                       (let ((goods (cdr (nth (- i 1) days-and-goods))))
+                         (if (null all-goods)
+                             goods
+                             (format nil "~a,~a~a" all-goods (if (= i 1) " and " " ") goods))))
+                     (loop for i from index
+                        downto 1
+                        collect i)
+                     :initial-value nil)))
+    (format nil
+            "~{~&~a~}"
+            (let ((begin (or begin 1))
+                  (end (or end (or begin 12))))
+              (loop for i from begin
+                 to end
+                 collect (verse i))))))
