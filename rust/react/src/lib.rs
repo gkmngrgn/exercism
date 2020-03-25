d1d123d5d5335cc1f7837a9529de2d90eea2f5a1
Gökmen Görgen <gkmngrgn@gmail.com>
2020-03-25 02:43:47 +0100

    rust, react: new solution.

--- rust/react/src/lib.rs
+++ rust/react/src/lib.rs
@@ -1,18 +1,49 @@
-use std::collections::HashMap;
+// This was the hardest puzzle on Exercism for me. I couldn't passed all the
+// unit tests so I got help from another solution. The original solution is
+// here:
+//
+// https://exercism.io/tracks/rust/exercises/react/solutions/296fb2da2de74b8b96e1d3b7f467ae04
+//
+// Note for me: The another way is to use Cell and RefCell instead of Rc.
+use std::rc::Rc;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+/// `InputCellID` is a unique identifier for an input cell.
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct InputCellID(usize);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+/// `ComputeCellID` is a unique identifier for a compute cell.
+/// Values of type `InputCellID` and `ComputeCellID` should not be mutually assignable,
+/// demonstrated by the following tests:
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input: react::ComputeCellID = r.create_input(111);
+/// ```
+///
+/// ```compile_fail
+/// let mut r = react::Reactor::new();
+/// let input = r.create_input(111);
+/// let compute: react::InputCellID = r.create_compute(&[react::CellID::Input(input)], |_| 222).unwrap();
+/// ```
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct ComputeCellID(usize);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct CallbackID(usize);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum CellID {
     Input(InputCellID),
     Compute(ComputeCellID),
+}
+
+impl From<CellID> for usize {
+    fn from(id: CellID) -> Self {
+        match id {
+            CellID::Input(InputCellID(i)) => i,
+            CellID::Compute(ComputeCellID(i)) => i,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -21,128 +52,181 @@ pub enum RemoveCallbackError {
     NonexistentCallback,
 }
 
-struct ComputeCell<'a, T> {
-    dependencies: Vec<CellID>,
-    compute_func: Box<dyn 'a + Fn(&[T]) -> T>,
+struct InputCell<T> {
+    parents: Rc<Vec<ComputeCellID>>,
+    value: T,
 }
 
+type Callback<'a, T> = Box<dyn FnMut(T) + 'a>;
+
+struct ComputeCell<'a, T> {
+    parents: Rc<Vec<ComputeCellID>>,
+    dependencies: Vec<CellID>,
+    callbacks: Vec<Option<Callback<'a, T>>>,
+    compute_func: Box<dyn Fn(&[T]) -> T + 'a>,
+    value: T,
+}
+
+impl<'a, T> ComputeCell<'a, T> {
+    fn value(&self, cells: &[Cell<T>]) -> Option<T>
+    where
+        T: Copy,
+    {
+        let dependencies: Option<Vec<T>> = self
+            .dependencies
+            .iter()
+            .map(|id| {
+                cells.get(usize::from(*id)).and_then(|cell| match cell {
+                    Cell::Input(c) => Some(c.value),
+                    Cell::Compute(c) => c.value(cells),
+                })
+            })
+            .collect();
+        Some((self.compute_func)(&dependencies?))
+    }
+}
+
+enum Cell<'a, T> {
+    Input(InputCell<T>),
+    Compute(ComputeCell<'a, T>),
+}
+
+#[derive(Default)]
 pub struct Reactor<'a, T> {
-    input_cells: HashMap<InputCellID, T>,
-    compute_cells: HashMap<ComputeCellID, ComputeCell<'a, T>>,
-    compute_callback_ids: HashMap<ComputeCellID, Vec<CallbackID>>,
-    callbacks: HashMap<CallbackID, Box<dyn 'a + FnMut(T) -> ()>>,
-    dependencies: HashMap<CellID, Vec<ComputeCellID>>,
+    cells: Vec<Cell<'a, T>>,
 }
 
 impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
     pub fn new() -> Self {
-        Self {
-            input_cells: HashMap::new(),
-            compute_cells: HashMap::new(),
-            compute_callback_ids: HashMap::new(),
-            callbacks: HashMap::new(),
-            dependencies: HashMap::new(),
-        }
+        Self { cells: vec![] }
     }
 
     pub fn create_input(&mut self, initial: T) -> InputCellID {
-        let input_cell_id = InputCellID(self.input_cells.len());
-        self.input_cells.insert(input_cell_id, initial);
+        let input_cell_id = InputCellID(self.cells.len());
+        let input_cell = InputCell {
+            parents: Rc::new(vec![]),
+            value: initial,
+        };
+        self.cells.push(Cell::Input(input_cell));
         input_cell_id
     }
 
-    pub fn create_compute<F: 'a + Fn(&[T]) -> T>(
+    pub fn create_compute<F: Fn(&[T]) -> T + 'a>(
         &mut self,
         dependencies: &[CellID],
         compute_func: F,
     ) -> Result<ComputeCellID, CellID> {
-        let compute_cell_id = ComputeCellID(self.compute_cells.len());
-        for dependency in dependencies {
-            if let None = self.value(*dependency) {
-                return Err(*dependency);
-            } else {
-                self.dependencies
-                    .entry(*dependency)
-                    .or_insert(vec![])
-                    .push(compute_cell_id);
-            }
+        let compute_cell_id = ComputeCellID(self.cells.len());
+        let dependencies = dependencies
+            .iter()
+            .map(|&d| {
+                if usize::from(d) < self.cells.len() {
+                    Ok(d)
+                } else {
+                    Err(d)
+                }
+            })
+            .collect::<Result<Vec<CellID>, CellID>>()?;
+
+        for &dep in &dependencies {
+            let cell = self.cells.get_mut(usize::from(dep)).ok_or(dep)?;
+            let parents = match cell {
+                Cell::Input(c) => &mut c.parents,
+                Cell::Compute(c) => &mut c.parents,
+            };
+            Rc::get_mut(parents).unwrap().push(compute_cell_id);
         }
-        let compute_cell = ComputeCell {
-            dependencies: dependencies.to_vec(),
-            compute_func: Box::new(compute_func),
+
+        let value = {
+            let dependencies: Result<Vec<T>, CellID> = dependencies
+                .iter()
+                .map(|&id| {
+                    self.cells
+                        .get(usize::from(id))
+                        .and_then(|cell| match cell {
+                            Cell::Input(c) => Some(c.value),
+                            Cell::Compute(c) => c.value(&self.cells),
+                        })
+                        .ok_or(id)
+                })
+                .collect();
+            (compute_func)(&dependencies?)
         };
-        self.compute_cells.insert(compute_cell_id, compute_cell);
+
+        let compute_cell = ComputeCell {
+            parents: Rc::new(vec![]),
+            dependencies,
+            callbacks: vec![],
+            compute_func: Box::new(compute_func),
+            value,
+        };
+        self.cells.push(Cell::Compute(compute_cell));
         Ok(compute_cell_id)
     }
 
     pub fn value(&self, id: CellID) -> Option<T> {
-        match id {
-            CellID::Input(id) => self.input_cells.get(&id).cloned(),
-            CellID::Compute(id) => self.compute_value(id),
-        }
-    }
-
-    fn compute_value(&self, id: ComputeCellID) -> Option<T> {
-        if let Some(cell) = self.compute_cells.get(&id) {
-            let dependencies: Vec<_> = cell
-                .dependencies
-                .iter()
-                .filter_map(|&d| self.value(d))
-                .collect();
-            Some((cell.compute_func)(&dependencies))
-        } else {
-            None
+        let cell = self.cells.get(usize::from(id))?;
+        match cell {
+            Cell::Input(c) => Some(c.value),
+            Cell::Compute(c) => c.value(&self.cells),
         }
     }
 
     pub fn set_value(&mut self, id: InputCellID, new_value: T) -> bool {
-        match self.input_cells.get_mut(&id) {
-            Some(v) => {
-                if v != &new_value {
-                    *(v) = new_value;
-                }
-
-                let dependencies = self
-                    .dependencies
-                    .get(&CellID::Input(id))
-                    .map_or(vec![], |d| {
-                        d.iter()
-                            .map(|&d| (d, self.compute_value(d).unwrap()))
-                            .collect()
-                    });
-
-                for (compute_cell_id, value) in dependencies.into_iter() {
-                    let callback_ids = self
-                        .compute_callback_ids
-                        .entry(compute_cell_id)
-                        .or_default();
-                    for callback_id in callback_ids {
-                        let callback = self.callbacks.get_mut(&callback_id).unwrap();
-                        callback(value);
-                    }
-                }
-
+        match self.cells.get_mut(id.0) {
+            Some(Cell::Input(cell)) => {
+                (*cell).value = new_value;
+                let parents = Rc::clone(&cell.parents);
+                self.notify(&parents);
                 true
             }
-            None => false,
+            _ => false,
         }
     }
 
-    pub fn add_callback<F: 'a + FnMut(T) -> ()>(
+    fn notify(&mut self, parents: &[ComputeCellID]) -> Option<()> {
+        for parent in parents {
+            let new_value = match self.cells.get(parent.0)? {
+                Cell::Input(c) => c.value,
+                Cell::Compute(c) => c.value(&self.cells)?,
+            };
+
+            if let Cell::Compute(ComputeCell {
+                value,
+                callbacks,
+                parents,
+                ..
+            }) = self.cells.get_mut(parent.0)?
+            {
+                let parents = Rc::clone(parents);
+                if new_value != *value {
+                    *value = new_value;
+                    callbacks.iter_mut().for_each(|callback| {
+                        if let Some(callback) = callback {
+                            (callback)(new_value);
+                        }
+                    })
+                }
+                self.notify(&parents);
+            }
+        }
+
+        Some(())
+    }
+
+    pub fn add_callback<F: FnMut(T) -> () + 'a>(
         &mut self,
         id: ComputeCellID,
         callback: F,
     ) -> Option<CallbackID> {
-        if !self.compute_cells.contains_key(&id) {
-            return None;
+        match self.cells.get_mut(id.0)? {
+            Cell::Compute(c) => {
+                let callback_id = CallbackID(c.callbacks.len());
+                c.callbacks.push(Some(Box::new(callback)));
+                Some(callback_id)
+            }
+            _ => None,
         }
-        let callback_id = CallbackID(self.callbacks.len());
-        self.callbacks.insert(callback_id, Box::new(callback));
-        self.compute_callback_ids
-            .entry(id)
-            .or_insert(vec![])
-            .push(callback_id);
-        Some(callback_id)
     }
 
     pub fn remove_callback(
@@ -150,19 +234,18 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
         cell: ComputeCellID,
         callback: CallbackID,
     ) -> Result<(), RemoveCallbackError> {
-        if !self.compute_cells.contains_key(&cell) {
-            return Err(RemoveCallbackError::NonexistentCell);
+        if let Cell::Compute(c) = self
+            .cells
+            .get_mut(cell.0)
+            .ok_or(RemoveCallbackError::NonexistentCell)?
+        {
+            let callback = c
+                .callbacks
+                .get_mut(callback.0)
+                .filter(|cb| cb.is_some())
+                .ok_or(RemoveCallbackError::NonexistentCallback)?;
+            *callback = None;
         }
-        if !self.callbacks.contains_key(&callback) {
-            return Err(RemoveCallbackError::NonexistentCallback);
-        }
-        self.callbacks.remove(&callback);
-        let compute_callbacks = self.compute_callback_ids.get_mut(&cell).unwrap();
-        let callback_index = compute_callbacks
-            .iter()
-            .position(|c| *c == callback)
-            .unwrap();
-        compute_callbacks.remove(callback_index);
         Ok(())
     }
 }
