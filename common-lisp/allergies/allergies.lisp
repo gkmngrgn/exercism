b451635633be7bb72e161f51c9854e3d246b3e8d
Gökmen Görgen <gkmngrgn@gmail.com>
2020-03-31 23:43:31 +0200

    common-lisp, allergies: new solution.

---
+++ common-lisp/allergies/allergies.lisp
@@ -0,0 +1,28 @@
+(in-package #:cl-user)
+(defpackage #:allergies
+  (:use #:common-lisp)
+  (:shadow #:list)
+  (:export #:allergic-to-p #:list))
+
+(in-package #:allergies)
+
+(defvar *allergens* '(("eggs"         . 1)
+                      ("peanuts"      . 2)
+                      ("shellfish"    . 4)
+                      ("strawberries" . 8)
+                      ("tomatoes"     . 16)
+                      ("chocolate"    . 32)
+                      ("pollen"       . 64)
+                      ("cats"         . 128)))
+
+(defun allergic-to-p (score allergen)
+  (loop for a in *allergens*
+     when (string= (string-downcase allergen) (car a))
+     return (> (logand (cdr a) score) 0)))
+
+
+(defun list (score)
+  (mapcar #'car
+          (remove-if-not (lambda (a)
+                           (allergic-to-p score (car a)))
+                         *allergens*)))
