7797203a7fba2a40cdd4442654405ce2b505dca1
Gökmen Görgen <gkmngrgn@gmail.com>
2020-05-24 01:05:08 +0200

    common-lisp, binary: update solution.

--- common-lisp/binary/binary.lisp
+++ common-lisp/binary/binary.lisp
@@ -4,11 +4,12 @@
 
 (in-package :binary)
 
+(defparameter *base* 2)
+
 (defun to-decimal (input)
   (let ((binaries (reverse (mapcar #'digit-char-p (coerce input 'list))))
         (exponent 0))
     (reduce #'+ (loop for b in binaries
-                      if (and (not (null b))
-                              (< b 2))
-                        collect (* b (expt 2 exponent))
+                      if (and (not (null b)) (< b *base*))
+                        collect (* b (expt *base* exponent))
                         and do (setf exponent (1+ exponent))))))
