fe0650a6bbc7b5187d887e8fe8bd79430ac05e0f
Gökmen Görgen <gkmngrgn@gmail.com>
2020-05-31 17:16:40 +0200

    rust, collatz-conjecture: update solution.

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,17 +1,13 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    match n {
-        0 => None,
-        _ => {
-            let mut steps = 0;
-            let mut number = n;
-            while number != 1 {
-                number = match number % 2 {
-                    0 => number / 2,
-                    _ => number * 3 + 1,
-                };
-                steps += 1;
-            }
-            Some(steps)
+    let mut steps = 0;
+    let mut number = n;
+    loop {
+        match number {
+            0 => return None,
+            1 => return Some(steps),
+            n if n % 2 == 0 => number /= 2,
+            _ => number = number * 3 + 1,
         }
+        steps += 1;
     }
 }
