199f2eef6940b895de32cced815931d5938747d1
Gökmen Görgen <gkmngrgn@gmail.com>
2020-05-31 17:44:05 +0200

    simple refactoring.

--- dart/collatz-conjecture/lib/collatz_conjecture.dart
+++ dart/collatz-conjecture/lib/collatz_conjecture.dart
@@ -1,11 +1,11 @@
 class CollatzConjecture {
   int steps(int n) {
     if (n <= 0) throw new ArgumentError("Only positive numbers are allowed");
-    int count = 0;
+    int steps = 0;
     while (n != 1) {
       n = n % 2 == 0 ? n ~/ 2 : 3 * n + 1;
-      count += 1;
+      steps += 1;
     }
-    return count;
+    return steps;
   }
 }
