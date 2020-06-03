2a756637412504959f0fb6d1328170b7f54baf18
Gökmen Görgen <gkmngrgn@gmail.com>
2020-06-04 00:52:47 +0200

    common-lisp, triangle: new solution.

---
+++ common-lisp/triangle/triangle.lisp
@@ -0,0 +1,23 @@
+(in-package #:cl-user)
+(defpackage #:triangle
+  (:use #:cl)
+  (:export #:triangle))
+
+(in-package #:triangle)
+
+(defun triangle (i j k)
+  (let* ((sides (list i j k))
+         (different-sides (remove-duplicates sides))
+         (sum (reduce #'+ sides)))
+    (labels ((count-invalid-sides (counter side)
+               (if (or (zerop side) (<= (- sum side) side))
+                   (1+ counter)
+                   counter))
+             (different-sides ()
+               (if (zerop (reduce #'count-invalid-sides sides :initial-value 0))
+                   (length different-sides))))
+      (case (different-sides)
+        (1         :equilateral)
+        (2         :isosceles)
+        (3         :scalene)
+        (otherwise :illogical)))))
