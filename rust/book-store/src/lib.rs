fdda2dc1db3002cd2df7d8eed7a411c1b68accde
Gökmen Görgen <gkmngrgn@gmail.com>
2020-06-04 18:08:14 +0200

    rust, book-store: update solution.

--- rust/book-store/src/lib.rs
+++ rust/book-store/src/lib.rs
@@ -1,46 +1,35 @@
 pub fn lowest_price(books: &[u32]) -> u32 {
-    let book_types = books.iter().fold(vec![], |mut list, i| {
-        if !list.contains(i) {
-            list.push(*i)
-        }
-        list
-    });
+    let mut book_types = books.to_vec();
+    book_types.sort();
+    book_types.dedup();
     (1..=book_types.len())
-        .map(|max_size| {
-            get_sizes_of_grouped_books(books.to_vec(), max_size)
-                .iter()
-                .map(|&size| calculate_basket_price(size))
-                .sum()
-        })
+        .map(|size| calculate_group_price(books, size))
         .min()
-        .unwrap_or(0)
+        .unwrap_or_default()
 }
 
-fn get_sizes_of_grouped_books(books: Vec<u32>, size: usize) -> Vec<usize> {
+fn calculate_group_price(books: &[u32], size: usize) -> u32 {
     let mut sizes = vec![];
-    let mut books = books;
+    let mut books = books.to_vec();
     while !books.is_empty() {
         let (mut grouped, mut remaining) = (vec![], vec![]);
-        for book in &books {
-            if grouped.len() == size || grouped.contains(&book) {
-                remaining.push(*book);
-            } else {
-                grouped.push(book);
-            }
-        }
+        books.iter().for_each(|book| match book {
+            b if grouped.len() == size || grouped.contains(&b) => remaining.push(*b),
+            b => grouped.push(b),
+        });
         sizes.push(grouped.len());
         books = remaining;
     }
-    sizes
+    sizes.iter().map(|&s| calculate_price(s)).sum()
 }
 
-fn calculate_basket_price(size: usize) -> u32 {
-    let discount = match size {
+fn calculate_price(size: usize) -> u32 {
+    let discount: u32 = match size {
         1 => 100,
         2 => 95,
         3 => 90,
         4 => 80,
         _ => 75,
     };
-    (size * 8 * discount) as u32
+    size as u32 * 8 * discount
 }
