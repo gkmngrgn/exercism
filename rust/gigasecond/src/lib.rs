9e0fa5bebcc10c99c54e2e0c19b88b2e03d9c143
Gökmen Görgen <gkmngrgn@gmail.com>
2020-07-16 22:43:32 +0200

    rust, gigasecond: update solution.

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,5 @@
 use chrono::{DateTime, Duration, Utc};
 
-// Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    return start + Duration::seconds(i64::pow(10, 9));
+    start + Duration::seconds(i64::pow(10, 9))
 }
