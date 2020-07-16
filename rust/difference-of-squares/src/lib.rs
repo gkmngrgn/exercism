b356321b0067c9394cc37e80526ca7cd75593a58
Gökmen Görgen <gkmngrgn@gmail.com>
2020-07-16 22:21:17 +0200

    rust, difference-of-squares: update solution.

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    return (1..n + 1).fold(0, |a, b| a + b).pow(2);
+    (1..=n).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    return (1..n + 1).fold(0, |a, b| a + b.pow(2));
+    (1..=n).fold(0, |a, b| a + b.pow(2))
 }
 
 pub fn difference(n: u32) -> u32 {
-    return square_of_sum(n) - sum_of_squares(n);
+    square_of_sum(n) - sum_of_squares(n)
 }
