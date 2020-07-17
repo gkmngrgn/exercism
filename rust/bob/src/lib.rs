16388c736845c6e1232d51aeaa6e73290977d79b
Gökmen Görgen <gkmngrgn@gmail.com>
2020-07-17 20:24:40 +0200

    rust, bob: update solution.

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,18 +1,18 @@
 pub fn reply(message: &str) -> &str {
+    let answer;
     let msg: String = message
         .trim()
         .chars()
         .filter(|x| x.is_alphabetic() || *x == '?')
         .collect();
-    let answer;
 
     if message.trim() == "" {
         answer = "Fine. Be that way!";
-    } else if msg.len() > 1 && msg.ends_with("?") && msg.to_uppercase() == msg {
+    } else if msg.len() > 1 && msg.ends_with('?') && msg.to_uppercase() == msg {
         answer = "Calm down, I know what I'm doing!";
-    } else if msg.ends_with("?") {
+    } else if msg.ends_with('?') {
         answer = "Sure.";
-    } else if msg.len() > 0 && msg.to_uppercase() == msg {
+    } else if !msg.is_empty() && msg.to_uppercase() == msg {
         answer = "Whoa, chill out!";
     } else {
         answer = "Whatever.";
