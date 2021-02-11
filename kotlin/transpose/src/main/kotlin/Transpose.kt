5f20b745e2418d24227e9048de4e902987577218
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-11 10:15:52 +0100

    kotlin, transpose: new solution (pseudocode)

--- kotlin/transpose/src/main/kotlin/Transpose.kt
+++ kotlin/transpose/src/main/kotlin/Transpose.kt
@@ -1,6 +1,29 @@
-object Transpose {
+import kotlin.math.max
 
+object Transpose {
     fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
+        // FIXME: pseudocode
+        val height = input.size
+        var width = 0
+        for (i in input) {
+            width = max(width, i.length)
+        }
+
+        val result = mutableListOf<String>()
+        for (x in 0 until width) {
+            val chars = mutableListOf<Char?>()
+            for (y in 0 until height) {
+                val v = input[y].getOrNull(x)
+                chars.add(v)
+            }
+            result.add(convertToLine(chars))
+        }
+
+        return result
+    }
+
+    private fun convertToLine(chars: List<Char?>): String {
+        val len = chars.count() - chars.takeLastWhile { it == null }.count()
+        return chars.take(len).map { it ?: ' ' }.joinToString(separator = "")
     }
 }
