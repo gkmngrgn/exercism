52ea6791bb884e5a7d6c495483843fec0768a46b
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-13 00:24:57 +0100

    kotlin, rna-transcription: new solution.

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,4 @@
-fun transcribeToRna(dna: String): String = ""
+const val DNA_CODES: String = "GCTA"
+const val RNA_CODES: String = "CGAU"
+
+fun transcribeToRna(dna: String): String = dna.map { RNA_CODES[DNA_CODES.indexOf(it)] }.joinToString(separator = "")
