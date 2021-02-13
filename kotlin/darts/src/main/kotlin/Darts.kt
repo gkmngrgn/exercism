c594e288facefaf6da5fb04d3688c793de6ffb69
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-13 14:06:10 +0100

    kotlin, darts: new solution.

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,22 @@
-object Darts {
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+object Darts {
+    fun <T> score(x: T, y: T): Int
+        where T : Number {
+        return when (sqrt(getValue(x).pow(2.0) + getValue(y).pow(2.0))) {
+            in 0.0..1.0 -> 10
+            in 1.0..5.0 -> 5
+            in 5.0..10.0 -> 1
+            else -> 0
+        }
+    }
+
+    fun getValue(n: Number): Double {
+        // FIXME
+        return when (n is Int) {
+            true -> n.toDouble()
+            else -> n as Double
+        }
     }
 }
