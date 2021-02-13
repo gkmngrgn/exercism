3a4a21f09e3deba78d62bd0f5e8eb626aee3119c
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-14 00:43:16 +0100

    kotlin, hamming: new solution.

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,13 @@
 object Hamming {
-
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+        return leftStrand.zip(rightStrand) { l, r ->
+            when (l) {
+                r -> 0
+                else -> 1
+            }
+        }.sum()
     }
 }
--- kotlin/hamming/src/test/kotlin/HammingTest.kt
+++ kotlin/hamming/src/test/kotlin/HammingTest.kt
@@ -1,11 +1,9 @@
-import org.junit.Test
-import org.junit.Ignore
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
 
 class HammingTest {
-
     @Rule
     @JvmField
     var expectedException: ExpectedException = ExpectedException.none()
@@ -15,31 +13,26 @@ class HammingTest {
         assertEquals(0, Hamming.compute("", ""))
     }
 
-    @Ignore
     @Test
     fun `single letter identical strands`() {
         assertEquals(0, Hamming.compute("A", "A"))
     }
 
-    @Ignore
     @Test
     fun `single letter different strands`() {
         assertEquals(1, Hamming.compute("G", "T"))
     }
 
-    @Ignore
     @Test
     fun `long identical strands`() {
         assertEquals(0, Hamming.compute("GGACTGAAATCTG", "GGACTGAAATCTG"))
     }
 
-    @Ignore
     @Test
     fun `long different strands`() {
         assertEquals(9, Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"))
     }
 
-    @Ignore
     @Test
     fun `disallow first strand longer`() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -48,7 +41,6 @@ class HammingTest {
         Hamming.compute("AATG", "AAA")
     }
 
-    @Ignore
     @Test
     fun `disallow second strand longer`() {
         expectedException.expect(IllegalArgumentException::class.java)
