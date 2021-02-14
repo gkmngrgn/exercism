6cabcf1d0a6832bd533c5fe73c0c724ba5d68ed2
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-14 13:44:07 +0100

    kotlin, custom-set: new solution.

--- kotlin/custom-set/src/main/kotlin/CustomSet.kt
+++ kotlin/custom-set/src/main/kotlin/CustomSet.kt
@@ -1,40 +1,30 @@
-class CustomSet() {
+class CustomSet(vararg initials: Int) {
+    private var list: IntArray = initials
 
-    // TODO: implement proper constructor
+    fun isEmpty(): Boolean = list.isEmpty()
 
-    fun isEmpty(): Boolean {
-        TODO("Implement this function to complete the task")
-    }
+    fun isSubset(other: CustomSet): Boolean = isEmpty() || list.none { !other.contains(it) }
 
-    fun isSubset(other: CustomSet): Boolean {
-        TODO("Implement this function to complete the task")
-    }
+    fun isDisjoint(other: CustomSet): Boolean = isEmpty() || list.all { !other.contains(it) }
 
-    fun isDisjoint(other: CustomSet): Boolean {
-        TODO("Implement this function to complete the task")
-    }
-
-    fun contains(other: Int): Boolean {
-        TODO("Implement this function to complete the task")
-    }
+    fun contains(other: Int): Boolean = list.contains(other)
 
     fun intersection(other: CustomSet): CustomSet {
-        TODO("Implement this function to complete the task")
+        val initials = list.intersect(other.list.asIterable()).toIntArray()
+        return CustomSet(*initials)
     }
 
-    fun add(other: Int) {
-        TODO("Implement this function to complete the task")
+    fun add(other: Int) = list.plus(other).also { list = it }
+
+    override fun equals(other: Any?): Boolean = when (other) {
+        is CustomSet -> isSubset(other) && other.isSubset(this)
+        else -> false
     }
 
-    override fun equals(other: Any?): Boolean {
-        TODO("Implement this function to complete the task")
-    }
+    override fun hashCode(): Int = list.contentHashCode()
 
-    operator fun plus(other: CustomSet): CustomSet {
-        TODO("Implement this function to complete the task")
-    }
+    operator fun plus(other: CustomSet): CustomSet = CustomSet(*list.plus(other.list.filter { !list.contains(it) }))
 
-    operator fun minus(other: CustomSet): CustomSet {
-        TODO("Implement this function to complete the task")
-    }
+    operator fun minus(other: CustomSet): CustomSet = CustomSet(*list.filter { !other.contains(it) }.toIntArray())
+
 }
