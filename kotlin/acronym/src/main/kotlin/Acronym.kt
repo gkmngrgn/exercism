ccaebf4cdb15819904e0f928128afbcaa20dda8c
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-15 00:51:25 +0100

    kotlin, acronym: new solution.

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,5 +1,8 @@
 object Acronym {
-    fun generate(phrase: String) : String {
-        TODO("Implement the function to complete the task")
-    }
+    private val ignores: CharArray = charArrayOf(' ', '-', '_')
+
+    fun generate(phrase: String): String =
+        " $phrase".windowed(2)
+            .filter { it[1].isLetter() && ignores.contains(it[0]) || it[1].isUpperCase() && it[0].isLowerCase() }
+            .map { it[1] }.joinToString(separator = "").toUpperCase()
 }
