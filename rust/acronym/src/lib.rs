6cd7010dafbc5ec9edcb52f71983801374c2d8ff
Gökmen Görgen <gkmngrgn@gmail.com>
2021-02-15 00:05:37 +0100

    rust, acronym: update solution.

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,13 +1,15 @@
+const IGNORES: [char; 3] = [' ', '-', '_'];
+
 pub fn abbreviate(phrase: &str) -> String {
-    let mut acronym = String::new();
-    let mut prev = ' ';
-    for p in phrase.chars() {
-        if p.is_alphabetic() && [' ', '-'].iter().any(|x| x == &prev)
-            || (p.is_uppercase() && prev.is_lowercase())
-        {
-            acronym.push(p);
-        }
-        prev = p;
-    }
+    let acronym: String = format!(" {}", phrase)
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .filter(|p| {
+            p[1].is_alphabetic() && IGNORES.iter().any(|x| x == &p[0])
+                || (p[1].is_uppercase() && p[0].is_lowercase())
+        })
+        .map(|p| p[1])
+        .collect();
     acronym.to_uppercase()
 }
--- rust/acronym/tests/acronym.rs
+++ rust/acronym/tests/acronym.rs
@@ -1,5 +1,3 @@
-use acronym;
-
 #[test]
 fn empty() {
     assert_eq!(acronym::abbreviate(""), "");
@@ -26,12 +24,7 @@ fn punctuation() {
 }
 
 #[test]
-fn all_caps_words() {
-    assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
-}
-
-#[test]
-fn non_acronym_all_caps_word() {
+fn all_caps_word() {
     assert_eq!(
         acronym::abbreviate("GNU Image Manipulation Program"),
         "GIMP"
@@ -39,9 +32,42 @@ fn non_acronym_all_caps_word() {
 }
 
 #[test]
-fn hyphenated() {
+fn all_caps_word_with_punctuation() {
+    assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
+}
+
+#[test]
+fn punctuation_without_whitespace() {
     assert_eq!(
         acronym::abbreviate("Complementary metal-oxide semiconductor"),
         "CMOS"
     );
+}
+
+#[test]
+fn very_long_abbreviation() {
+    assert_eq!(
+        acronym::abbreviate(
+            "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"
+        ),
+        "ROTFLSHTMDCOALM"
+    );
+}
+
+#[test]
+fn consecutive_delimiters() {
+    assert_eq!(
+        acronym::abbreviate("Something - I made up from thin air"),
+        "SIMUFTA"
+    );
+}
+
+#[test]
+fn apostrophes() {
+    assert_eq!(acronym::abbreviate("Halley's Comet"), "HC");
+}
+
+#[test]
+fn underscore_emphasis() {
+    assert_eq!(acronym::abbreviate("The Road _Not_ Taken"), "TRNT");
 }
